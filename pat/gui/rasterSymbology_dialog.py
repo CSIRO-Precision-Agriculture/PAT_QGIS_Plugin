2f4d4a66dc8df8a4a5a5316e56bf7e66857b220d
Christina Ratcliff <29470818+cratcliff@users.noreply.github.com>
2021-10-29 09:47:16 +1030

    convert OK to APPLY to keep dialog open

--- pat/gui/rasterSymbology_dialog.py
+++ pat/gui/rasterSymbology_dialog.py
@@ -84,6 +84,8 @@ class RasterSymbologyDialog(QDialog, FORM_CLASS):
 
         # GUI Customisation -----------------------------------------------
         self.mcboTargetLayer.setFilters(QgsMapLayerProxyModel.RasterLayer)
+        self.mcboTargetLayer.setExcludedProviders(['wms'])
+
         # self.setMapLayers()
         self.setWindowIcon(QtGui.QIcon(':/plugins/pat/icons/icon_rasterSymbology.svg'))
         self.cboType.addItems(list(rs.RASTER_SYMBOLOGY))
@@ -205,29 +207,21 @@ class RasterSymbologyDialog(QDialog, FORM_CLASS):
     def accept(self, *args, **kwargs):
         if not self.validate():
             return False
-        try:
-            rast_sym = rs.RASTER_SYMBOLOGY[self.cboType.currentText()]
-            QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
-            if rast_sym['type'] == 'unique':
-                rs.raster_apply_unique_value_renderer(self.mcboTargetLayer.currentLayer(), 1,
-                                                      color_ramp=rast_sym['colour_ramp'],
-                                                      invert=rast_sym['invert'])
-            else:
 
-                rs.raster_apply_classified_renderer(self.mcboTargetLayer.currentLayer(),
-                                                    rend_type=rast_sym['type'],
-                                                    num_classes=rast_sym['num_classes'],
-                                                    color_ramp=rast_sym['colour_ramp'])
+        rast_sym = rs.RASTER_SYMBOLOGY[self.cboType.currentText()]
 
-            QApplication.restoreOverrideCursor()
-            self.iface.mainWindow().statusBar().clearMessage()
-            return super(RasterSymbologyDialog, self).accept(*args, **kwargs)
+        #QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
+        if rast_sym['type'] == 'unique':
+            rs.raster_apply_unique_value_renderer(self.mcboTargetLayer.currentLayer(), 1,
+                                                  color_ramp=rast_sym['colour_ramp'],
+                                                  invert=rast_sym['invert'])
+        else:
 
-        except Exception as err:
-            QApplication.restoreOverrideCursor()
-            self.cleanMessageBars(True)
-            self.iface.mainWindow().statusBar().clearMessage()
+            rs.raster_apply_classified_renderer(self.mcboTargetLayer.currentLayer(),
+                                                rend_type=rast_sym['type'],
+                                                num_classes=rast_sym['num_classes'],
+                                                color_ramp=rast_sym['colour_ramp'])
 
-            self.send_to_messagebar(str(err), level=Qgis.Critical,
-                                    duration=0, addToLog=True, exc_info=sys.exc_info())
-            return False  # leave dialog open
+        #QApplication.restoreOverrideCursor()
+        #self.iface.mainWindow().statusBar().clearMessage()
+        return False #super(RasterSymbologyDialog, self).accept(*args, **kwargs)
