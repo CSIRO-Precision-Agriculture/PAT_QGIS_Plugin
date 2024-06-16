0394ecd7d8ecfe83ff9e7efd9a2cb6bdb4d0095d
Birchall <bir122@csiro.au>
2024-06-17 09:02:32 +1000

    Test ZZ9 plural alpha 1.0

--- pat/pbt_ops.py
+++ pat/pbt_ops.py
@@ -1,7 +1,7 @@
 import os
 print("Executing pb_tool compile")
-os.system("pb_tool compile")
+os.system("cd pat; pb_tool compile")
 print("Executing pb_tool deploy")
-os.system("pb_tool deploy -y")
+os.system("cd pat; pb_tool deploy -y")
 print("Executing pb_tool zip")
-os.system("pb_tool zip")
+os.system("cd pat; pb_tool zip")
