03ce33db93041dc76fb062edb837d260ffbfa0e7
Birchall <bir122@csiro.au>
2024-06-26 11:17:24 +1000

    Added the pb_tools build and deploy operations as a python script to be called by github action workflows

---
+++ pat/pbt_ops.py
@@ -0,0 +1,11 @@
+import os
+print ("Current Directory =", os.getcwd)
+print("cd pat")
+os.chdir("pat")
+print ("Current Directory =", os.getcwd)
+print("Executing pb_tool compile")
+os.system("pb_tool compile")
+print("Executing pb_tool deploy")
+os.system("pb_tool deploy -y")
+print("Executing pb_tool zip")
+os.system("pb_tool zip")
